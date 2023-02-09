7f30ff8cc252d41f1d8a9c1a20f589119c8cf6c6
Intummadee <88524336+Intummadee@users.noreply.github.com>
2023-02-09 20:22:50 +0700

    Add files via upload

---
+++ WEEK04-ASYNC/exercise-02/answer.js
@@ -0,0 +1,21 @@
+// ข้อ 2.1
+
+function display(msg) {
+    let div = document.getElementById('ex01-div')
+    div.innerHTML = msg
+}
+
+function showConfirm(callback) {
+    // You code here
+}
+
+// ข้อ 2.2
+function start() {
+    // hint: ส่ง callback function เข้าไปเป็น argument ของ setTimeout()
+}
+
+// ข้อ 2.3
+function stopTime() {
+
+}
+
