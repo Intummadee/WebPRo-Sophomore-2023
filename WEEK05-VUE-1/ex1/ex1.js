a1fbbd77a9d8740605a6214a7cafe6eb69de1b58
Intummadee <88524336+Intummadee@users.noreply.github.com>
2023-02-17 11:49:15 +0700

    complete

---
+++ WEEK05-VUE-1/ex1/ex1.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el : '#ex1',
+    data: {
+        student_id : '64070257',
+        student_fname : 'Intummadee',
+        student_lname : 'Maliyam',
+        styleObj: {
+            color: 'black',
+            fontSize: '2em',
+        },
+
+        image : '<img src="./image/0.svg">',
+        // isActive : false
+    }
+})
